3f270d416b2f06921ebb699ba4617c1cce665bcc
asdf1163 <rafaszromek@gmail.com>
2021-10-18 16:13:22 +0200

    git commit

--- src/componets/context/Edit.tsx
+++ src/componets/context/Edit.tsx
@@ -1,14 +1,11 @@
 import { useState, createContext } from "react";
-
 interface Ichildren
 {
     children: React.ReactNode
 }
-
 export const EditContext = createContext<any>(0)
 
 export const EditProvider: React.FC<Ichildren> = ({ children }) => {
-
     const [productID, setProductID] = useState(0)
     const [arrayID, setArrayID] = useState(0)
 
